97142ff7ca2d6c828f938607eb7fbf876d7148af
rakell2726@hotmail.com <rakell2726@hotmail.com>
2022-05-17 23:42:52 -0500

    se agregan pagina html y js

---
+++ login.js
@@ -0,0 +1,17 @@
+function validar() {
+    var user = document.getElementById("usuario").value;
+    var password = document.getElementById("pasaporte");
+    if (user = usuario) {
+        textContent = "El usuario existe"
+    }
+}
+
+var lista = document.getElementById("lista-dinamica");
+const products = ["Rosas","Tulipanes","Claveles","Gerbera","Orquideas","Lilis","Alcatraces"]
+for (let i = 0; i < listaProducto.length; i++) {
+    const element = listaProducto
+    [i];
+    var li = document.createElement("li");
+    li.textContent = element;
+    lista.appendChild(li);
+}
